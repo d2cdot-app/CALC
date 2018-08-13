423f13d061eb178dca8cc263d81985980dc625e0
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-08-13 20:14:19 +0900

    計算ロジックの追加

--- js/main.js
+++ js/main.js
@@ -1,22 +1,47 @@
-var input = 0; //入力中数字保存変数
+var input = ''; //入力中数字保存変数
 var result =0;//計算結果保存変数
-var savaCalc =0;//選択された四則演算ボタン保存変数
+var savaCalc ='';//選択された四則演算ボタン保存変数
 
-function calcTest(X){
+function calc(X){
   if(isNaN(X)==false){
     input =(input*10) + X;
-  form.text.value=input;
+    form.text.value=input;
   }else{
     if(X== '+'){
-      result =result+input;
-      input = 0;
-      savaCalc ="+";
-      form.text.value=result + "aaa";
+      if(input!=''){
+        result = input;
+        input = '';
+        savaCalc ="+";
+      }
+    }else if(X== '-'){
+      if(input!=''){
+        result = input;
+        input = '';
+        savaCalc ="-";
+      }
+    }else if(X== '*'){
+      if(input!=''){
+        result = input;
+        input = '';
+        savaCalc ="*";
+      }
+    }else if(X== '/'){
+      if(input!=''){
+        result = input;
+        input = '';
+        savaCalc ="/";
+      }
     }else {
       if(X== '='){
-      result =result+input;
-      input = 0;
-      form.text.value=result;
+        if(input==''){
+
+        }else{
+          form.text.value=eval(result + savaCalc + input);
+          result = form.text.value;
+          input = '';
+        }
+
+
       }
     }
     }
