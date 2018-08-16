12cb22c813bd7d97c0f6e203af26868c3bc4fb31
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-08-16 15:51:57 +0900

    計算ロジックの修正

--- js/main.js
+++ js/main.js
@@ -1,49 +1,32 @@
-var input = ''; //入力中数字保存変数
-var result =0;//計算結果保存変数
-var savaCalc ='';//選択された四則演算ボタン保存変数
-//a
+var input = 0;
+var result = 0;
+var savaCalc =0;
 
 function calc(X){
   if(isNaN(X)==false){
     input =(input*10) + X;
     form.text.value=input;
   }else{
-    if(X== '+'){
-      result = result + input;
-      input = 0;
-      savaCalc ="+";
-
-      form.text.value=result;
-    }else if(X== '-'){
-      result = result - input;
-      input = 0;
-      savaCalc ="-";
-
-      form.text.value=result;
-    }else if(X== '*'){
-      if(input!=''){
+    switch (savaCalc) {
+      case 0:
         result = input;
-        input = '';
-        savaCalc ="*";
-      }
-    }else if(X== '/'){
-      if(input!=''){
-        result = input;
-        input = '';
-        savaCalc ="/";
-      }
-    }else if(X== '='){
-        if(input!=''){
-          form.text.value=eval(result + savaCalc + input);
-          result = form.text.value;
-          input = '';
-          savaCalc = '';
-        }
-      }else if(X=='C'){
-        input = '';
-        result = 0;
-        savaCalc ='';
-        form.text.value=result;
-      }
+        break;
+      case '+':
+        result = result + input;
+        break;
+      case '-':
+        result = result - input;
+        break;
+      case '*':
+        result = result * input;
+        break;
+      case '/':
+        result = result / input;
+        break;
+
     }
+    input = 0;
+    savaCalc = X;
+    form.text.value=result;
   }
+}
