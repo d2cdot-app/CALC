86e89f228606e11136a1e5beedb3978873f55ead
山本 沙季 <saki.yamamoto@d2cdot.co.jp>
2018-08-20 14:03:10 +0900

    計算ロジックの追加

--- js/main.js
+++ js/main.js
@@ -5,13 +5,21 @@ var savaCalc =0;
 function calc(X){
   if(isNaN(X)==false){
     input =(input*10) + X;
-    form.text.value=input;
+    form.text.value = input;
+  }else if(X=='='){
+    if(input!=0){
+      form.text.value=eval(result + savaCalc + input);
+      result = form.text.value;
+      input = 0;
+    }
+
+
   }else{
     switch (savaCalc) {
-      case 0:
+      case  0:
         result = input;
         break;
-      case '+':
+      case  '+':
         result = result + input;
         break;
       case '-':
@@ -23,10 +31,9 @@ function calc(X){
       case '/':
         result = result / input;
         break;
-
     }
     input = 0;
     savaCalc = X;
     form.text.value=result;
   }
-}
+  }
