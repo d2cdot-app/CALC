087f9bd8762de2d9422793c3e9983252af702e5b
[D2C dot] Kenichi Suzuki <kenichi.suzuki@d2cdot.co.jp>
2018-08-22 15:08:57 +0900

    no message

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var input = 0;
 var result = 0;
-var savaCalc =0;
+var saveCalc =0;
 
 function execCalc(value01,value02,operator){
   switch (operator) {
@@ -28,27 +28,22 @@ function calc(X){
     input =(input*10) + X;
     form.text.value = input;
   }else if(X=='='){
-    result = execCalc(result,input,savaCalc);
-    form.text.value=result;
-    // input = result;
-    // input = execCalc(result,input,savaCalc);
-    savaCalc = 0;
-    // result = 0;
+    result = execCalc(result,input,saveCalc);
+    form.text.value = result;
 
   }else if(X=='C'){
     input =0;
     result =0;
     savaCalc =0;
-    form.text.value=result;
   }else{
-    if(savaCalc ==0){
-      savaCalc = X;
+    if(saveCalc != 0){
+      result = execCalc(result,input,saveCalc);
       form.text.value=result;
     }else{
-      result = execCalc(result,input,savaCalc);
-      input = 0;
-      savaCalc = X;
-      form.text.value=result;
+      result = parseInt(form.text.value);
     }
+    input = 0;
+    saveCalc = X;
   }
+
 }
